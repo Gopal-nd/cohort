576facf4f9b7ef96279d44a86d26f61238b10ff1
Gopal-nd <gopalhelpyou@gmail.com>
2025-01-24 18:36:26 +0530

    learned rebase

--- Git/Projects/Rebase/Readme.md
+++ Git/Projects/Rebase/Readme.md
@@ -1,1 +1,3 @@
 Hello  this is me from the main 
+
+some thing new is going in the classic
---
+++ Git/Projects/Rebase/index.js
@@ -0,0 +1,13 @@
+console.log("hello from the main branch")
+
+for(let i =0 ;i<100;i++){
+  console.log(i)
+  console.log("\n")
+}
+
+console.log("lest make a todo applocation")
+console.log("Print a muliple number of 4")
+for(let i=0;i<10;i++){
+  console.log(`4 X ${i+1} = ${(i+1)*4} \t`)
+}
+
